502d6a28b08a148bc34f0a2419c563560b264af1
Cailey Jones <cailey.jones@gov.bc.ca>
2020-03-06 12:39:30 -0800

    make sure that you can actually keep doing offline stuff

--- test.js
+++ test.js
@@ -3,12 +3,11 @@ const getPackageInfo = require('./functions/get-package-info');
 const isOnline = require('./functions/online');
 
 (async () => {
-    console.log(await isOnline());
+    await isOnline()
     let packageInfoPromise = getPackageInfo('grizzly-bear-population-units');
     packageInfoPromise.then(function(packageInfo) {
         console.log(packageInfo);
     });
 })();
 
-
-
+console.log("continue doing the things");
